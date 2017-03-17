606276246795eb05ef31a503c019d099d4a6489a
twzjwang <twzjwang@gmail.com>
2017-03-18 03:17:22 +0800

    Establish exe files by separate functions
    generate 'naive_transpose', 'sse_transpose', 'sse_prefetch_transpose'
    files corresponding to with their own transpose functions in impl.c

--- Makefile
+++ Makefile
@@ -2,12 +2,33 @@ CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra
 
 GIT_HOOKS := .git/hooks/applied
 
-all: $(GIT_HOOKS) main.c
-	$(CC) $(CFLAGS) -o main main.c
+EXEC = naive_transpose sse_transpose sse_prefetch_transpose
 
+all: $(GIT_HOOKS) main.c $(EXEC)
+
+naive_transpose: main.c
+	$(CC) $(CFLAGS) -DNAI -o $@ main.c
+    
+sse_transpose: main.c
+	$(CC) $(CFLAGS) -DSSE -o $@ main.c
+    
+sse_prefetch_transpose: main.c
+	$(CC) $(CFLAGS) -DSSE_PF -o $@ main.c
+
+test: $(EXEC)
+	perf stat --repeat 5 \
+		-e cache-misses,cache-references,instructions,cycles \
+		./naive_transpose
+	perf stat --repeat 5 \
+		-e cache-misses,cache-references,instructions,cycles \
+		./sse_transpose
+	perf stat --repeat 5 \
+		-e cache-misses,cache-references,instructions,cycles \
+		./sse_prefetch_transpose  
+    
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
 	@echo
 
 clean:
-	$(RM) main
+	$(RM) $(EXEC)
--- main.c
+++ main.c
@@ -31,64 +31,32 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 
 int main()
 {
-    /* verify the result of 4x4 matrix */
-    {
-        int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                           8, 9, 10, 11, 12, 13, 14, 15
-                         };
-        int testout[16];
-        int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                             2, 6, 10, 14, 3, 7, 11, 15
-                           };
+    struct timespec start, end;
+    int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+    int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
 
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testin[y * 4 + x]);
-            printf("\n");
-        }
-        printf("\n");
-        sse_transpose(testin, testout, 4, 4);
-        for (int y = 0; y < 4; y++) {
-            for (int x = 0; x < 4; x++)
-                printf(" %2d", testout[y * 4 + x]);
-            printf("\n");
-        }
-        assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
-               "Verification fails");
-    }
-
-    {
-        struct timespec start, end;
-        int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-
-        srand(time(NULL));
-        for (int y = 0; y < TEST_H; y++)
-            for (int x = 0; x < TEST_W; x++)
-                *(src + y * TEST_W + x) = rand();
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse: \t\t %ld us\n", diff_in_us(start, end));
-
-        clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("naive: \t\t %ld us\n", diff_in_us(start, end));
-
-        free(src);
-        free(out0);
-        free(out1);
-        free(out2);
-    }
+    srand(time(NULL));
+    for (int y = 0; y < TEST_H; y++)
+        for (int x = 0; x < TEST_W; x++)
+            *(src + y * TEST_W + x) = rand();
+#if defined SSE_PF
+    clock_gettime(CLOCK_REALTIME, &start);
+    sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
+#elif defined SSE
+    clock_gettime(CLOCK_REALTIME, &start);
+    sse_transpose(src, out0, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("sse: \t\t %ld us\n", diff_in_us(start, end));
+#elif defined NAI
+    clock_gettime(CLOCK_REALTIME, &start);
+    naive_transpose(src, out0, TEST_W, TEST_H);
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("naive: \t\t %ld us\n", diff_in_us(start, end));
+#endif
+    free(src);
+    free(out0);
 
     return 0;
 }
